996e57f7bd0af559f2eb41e6c7d3edc6a561ae67
Damlare Adeosun <dami4adeosun@icloud.com>
2025-10-05 13:58:10 +0100

    Initial Commit

---
+++ Pocket Balance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pocket Balance
+//
+//  Created by Damilare Adeosun on 05/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pocket Balance/Pocket_BalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_BalanceApp.swift
+//  Pocket Balance
+//
+//  Created by Damilare Adeosun on 05/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_BalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
